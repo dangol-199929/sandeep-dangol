4a5f880224165204325a7472b0ae8101e4f46bea
Sandeep Dangol <sandeepdangol1999sep29@gmail.com>
2026-01-29 00:15:08 +1100

    chore: thumbnail image

--- imageconfig.ts
+++ imageconfig.ts
@@ -1,3 +1,5 @@
+import og from "./public/og-image.png";
+
 import me from "./public/me/pic.png";
 
 import bs from "./public/projects/basestation.png";
@@ -5,6 +7,8 @@ import revv from "./public/projects/revv.png";
 import koklass from "./public/projects/koklass.png";
 import ban from "./public/projects/phone.png";
 import assistant from "./public/projects/assistant.png";
+
+export const OG = og;
 
 export const ME = me;
 
